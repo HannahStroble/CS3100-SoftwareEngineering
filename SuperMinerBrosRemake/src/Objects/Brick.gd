5e8c0c9fff158d8e98de01b6718468cc1c759a6f
Greg Hilmes <99hilmes.g@gmail.com>
2020-04-06 19:00:28 -0500

    Draft coinboxes.

--- SuperMinerBrosRemake/src/Objects/Brick.gd
+++ SuperMinerBrosRemake/src/Objects/Brick.gd
@@ -1,8 +1,25 @@
 extends StaticBody2D
 
+var numCoins = 0
+
+func set_coins(coins : int):
+	numCoins = coins
+
 
 func _on_BrickHitBox_body_entered(body):
-	#get_node("brickSprite").hide() #used to change the block to a dead box upon impact
-	queue_free() #used to delete the block upon impact
+	if numCoins > 0:
+		# Add a new coin
+		var coin = load("res://src/Objects/Coin.tscn").instance()
+		get_parent().add_child(coin)
+		# set position
+		coin.position.x = body.global_position.x
+		coin.position.y = body.global_position.y
+
+		numCoins -= 1
+		
+	if numCoins == 0:
+		get_node("brickSprite").hide() #used to change the block to a dead box upon impact
+		# queue_free() #used to delete the block upon impact
+		
 	return
 
